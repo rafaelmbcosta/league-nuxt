61068c1660321dd3bddb00aefb7ed78b44d9ab3c
Zora Santos <zoranildo22@gmail.com>
2021-05-27 00:26:44 -0300

    Cria tela de cadastro por tag

---
+++ pages/tag.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+   <v-col>
+      <h2 class="mb-8">ADCIONAR PARTICIPANTE PELA TAG</h2>
+      <v-card class="left-border">
+        <v-card-text>
+          <v-text-field label="TAG"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary">Adicionar</v-btn>
+        </v-card-actions>
+      </v-card>
+   </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'tag'
+
+}
+</script>
+
+<style>
+  .left-border {
+    border-left: 8px solid #008172 !important;
+  }
+</style>
