addd7688e527c893bd29baed0e3b67e68f8a7422
Zora Santos <zoranildo22@gmail.com>
2021-05-29 01:38:10 -0300

    Faz requisição do cadastro pela tag

--- pages/tag.vue
+++ pages/tag.vue
@@ -2,13 +2,16 @@
   <v-container>
    <v-col>
       <h2 class="mb-8">ADCIONAR PARTICIPANTE PELA TAG</h2>
+      {{ typeof idTag }}
       <v-card class="left-border">
         <v-card-text>
-          <v-text-field label="TAG"
+          <v-text-field
+           label="TAG"
+           v-model.number="idTag"
           />
         </v-card-text>
         <v-card-actions>
-          <v-btn color="primary">Adicionar</v-btn>
+          <v-btn @click="saveMember" color="primary">Adicionar</v-btn>
         </v-card-actions>
       </v-card>
    </v-col>
@@ -16,8 +19,34 @@
 </template>
 
 <script>
+import gql from 'graphql-tag'
 export default {
-  name: 'tag'
+  name: 'tag',
+  data: () => {
+    return {
+      idTag: null
+    }
+  },
+  methods: {
+    async saveMember() {
+      try {
+        await this.$apollo.mutate({
+          mutation: gql`mutation($teamId: Int!) {
+            createTeamById(teamId: $teamId) {
+              id
+              idTag
+              playerName
+            }
+          }`,
+          variables: {
+            teamId: this.idTag
+          },
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 
 }
 </script>
